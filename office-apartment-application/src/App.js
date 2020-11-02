888608f43aba00a7e0ae74313eb169db245f055c
Adam Feather <afeather@thinklogic.net>
2020-11-01 16:39:26 -0800

    aded files

---
+++ office-apartment-application/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Map from './Components/MapComponent/Map'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+    
+      </header>
+      <Map style='width: 400px; height: 300px;'/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ office-apartment-application/src/Components/MapComponent/Map.js
@@ -0,0 +1,64 @@
+import React, {Component} from "react"
+import "../../App.css"
+import mapboxgl  from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWZlYXRoZXIyMCIsImEiOiJjazhmaDlrYmEwNDg2M2dzMHRycG4wMXJzIn0._FYX6dOkYeSWZTCyQtZs0w';
+
+class Map extends Component { 
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            lng: 5,
+            lat: 34,
+            zoom: 2,
+            isLoaded: false
+        };
+    }
+    omponentDidMount() {
+        const map = new mapboxgl.Map({
+            container: this.mapContainer,
+            style: 'mapbox://styles/mapbox/streets-v11?optimize=true',
+            center: [this.state.lng, this.state.lat],
+            zoom: this.state.zoom
+        });
+        this.map = map;
+
+        let geocoder = new MapboxGeocoder({
+            accessToken: mapboxgl.accessToken,
+            mapboxgl: mapboxgl
+            });
+
+        // Add geolocate control to the map.
+        this.map.addControl(
+            new mapboxgl.GeolocateControl({
+            positionOptions: {
+            enableHighAccuracy: true
+            },
+            trackUserLocation: true
+            })
+        );
+
+        document.getElementById('geocoder').appendChild(geocoder.onAdd(this.map));
+        this.setState({
+            isLoaded: true
+        });
+
+    }
+
+    render() {
+        return (
+            <div>
+
+                
+                    <div id="geocoder" className="geocoder"></div>
+                    <div ref={el => this.mapContainer = el} className="mapContainer"  > </div>
+
+            </div>
+        );
+    }
+
+}
+
+
+export default Map
