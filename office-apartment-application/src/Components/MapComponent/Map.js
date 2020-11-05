a0452b9f939e7a08e503e0128d49774b00c36ba6
Matt Kot <mekot16@gmail.com>
2020-11-04 20:37:18 -0500

    [main] added markers to the map with a click event as a proof of concept

--- office-apartment-application/src/Components/MapComponent/Map.js
+++ office-apartment-application/src/Components/MapComponent/Map.js
@@ -4,7 +4,7 @@ import mapboxgl from 'mapbox-gl';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWZlYXRoZXIyMCIsImEiOiJjazhmaDlrYmEwNDg2M2dzMHRycG4wMXJzIn0._FYX6dOkYeSWZTCyQtZs0w';
 
-class Map extends Component { 
+class Map extends Component {
 
     constructor(props) {
         super(props);
@@ -12,7 +12,7 @@ class Map extends Component {
             lng: 5,
             lat: 34,
             zoom: 2,
-            isLoaded: false
+            isLoaded: false,
         };
     }
 
@@ -40,10 +40,62 @@ class Map extends Component {
         );
 
         document.getElementById('geocoder').appendChild(geocoder.onAdd(this.map));
+        
+        const mapPointJson = this.getMapPointData();
+        mapPointJson.features.forEach((point) => {
+            console.log("point");
+            // build element
+            let el = document.createElement('div');
+            el.className = 'marker';
+            el.style.width = '10px';
+            el.style.height = '10px';
+            el.style.background = 'blue';
+
+            el.addEventListener('click', () => {
+                window.alert("It worked! "+point.properties.label);
+            });
+
+            const options = { element: el };
+            // add element to map
+            new mapboxgl.Marker(options)
+                .setLngLat(point.geometry.coordinates)
+                .addTo(this.map);
+        });
+        
+
         this.setState({
             isLoaded: true
         });
 
+    }
+
+    // TODO- this is just for some test data, but we'll probably want some type of structure like this
+    getMapPointData() {
+        return {
+            'type': 'Feature',
+            'features': [
+                {
+                    'properties': {
+                        'id': 1,
+                        'label': 'House 1',
+                    },
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': [-78.836754, 40.242299], // Note: long, lat
+                    }
+                },
+                {
+                    'properties': {
+                        'id': 2,
+                        'label': 'Building 2',
+                    },
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': [-78.828488, 40.229427], // Note: long, lat
+                    }
+                },
+            ]
+        };
     }
 
     render() {
