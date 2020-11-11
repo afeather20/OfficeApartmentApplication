6a373710b4d840cb8cd258f969dcb3459e28e365
Matt Kot <mekot16@gmail.com>
2020-11-11 15:53:55 -0500

    [main] converting file indentations from spaces to tabs

--- office-apartment-application/src/App.js
+++ office-apartment-application/src/App.js
@@ -3,16 +3,16 @@ import './App.css';
 import Map from './Components/MapComponent/Map'
 
 function App() {
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-    
-      </header>
-      <Map />
 
-    </div>
-  );
+	return (
+		<div className="App">
+		<header className="App-header">
+		
+		</header>
+		<Map />
+
+		</div>
+	);
 }
 
 export default App;
--- office-apartment-application/src/Components/MapComponent/Map.js
+++ office-apartment-application/src/Components/MapComponent/Map.js
@@ -11,106 +11,106 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYWZlYXRoZXIyMCIsImEiOiJjazhmaDlrYmEwNDg2M2dzM
 
 class Map extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            lng: 5,
-            lat: 34,
-            zoom: 2,
-            isLoaded: false,
-            currentSelection: null,
-        };
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			lng: 5,
+			lat: 34,
+			zoom: 2,
+			isLoaded: false,
+			currentSelection: null,
+		};
+	}
 
-    componentDidMount() {
-        this.map = new mapboxgl.Map({
-            container: this.mapContainer,
-            style: 'mapbox://styles/mapbox/streets-v11?optimize=true',
-            center: [this.state.lng, this.state.lat],
-            zoom: this.state.zoom
-        });
+	componentDidMount() {
+		this.map = new mapboxgl.Map({
+			container: this.mapContainer,
+			style: 'mapbox://styles/mapbox/streets-v11?optimize=true',
+			center: [this.state.lng, this.state.lat],
+			zoom: this.state.zoom
+		});
 
-        let geocoder = new MapboxGeocoder({
-            accessToken: mapboxgl.accessToken,
-            mapboxgl: mapboxgl
-        });
+		let geocoder = new MapboxGeocoder({
+			accessToken: mapboxgl.accessToken,
+			mapboxgl: mapboxgl
+		});
 
-        // Add geolocate control to the map.
-        this.map.addControl(
-            new mapboxgl.GeolocateControl({
-                positionOptions: {
-                    enableHighAccuracy: true
-                },
-                trackUserLocation: true
-            })
-        );
+		// Add geolocate control to the map.
+		this.map.addControl(
+			new mapboxgl.GeolocateControl({
+				positionOptions: {
+					enableHighAccuracy: true
+				},
+				trackUserLocation: true
+			})
+		);
 
-        document.getElementById('geocoder').appendChild(geocoder.onAdd(this.map));
-        
-        const mapPointJson = this.getMapPointData();
-        mapPointJson.features.forEach((point) => {
-            // build element
-            const el = document.createElement('div');
-            ReactDOM.render(<MarkerWrapper text={point.properties.label} />, el);
+		document.getElementById('geocoder').appendChild(geocoder.onAdd(this.map));
+		
+		const mapPointJson = this.getMapPointData();
+		mapPointJson.features.forEach((point) => {
+			// build element
+			const el = document.createElement('div');
+			ReactDOM.render(<MarkerWrapper text={point.properties.label} />, el);
 
-            // add element to map
-            const options = { element: el };
-            new mapboxgl.Marker(options)
-                .setLngLat(point.geometry.coordinates)
-                .addTo(this.map);
-        });
+			// add element to map
+			const options = { element: el };
+			new mapboxgl.Marker(options)
+				.setLngLat(point.geometry.coordinates)
+				.addTo(this.map);
+		});
 
-        this.setState({
-            isLoaded: true
-        });
+		this.setState({
+			isLoaded: true
+		});
 
-        this.map.on('move', () => {
-            this.setState({
-                lng: this.map.getCenter().lng.toFixed(4),
-                lat: this.map.getCenter().lat.toFixed(4),
-                zoom: this.map.getZoom().toFixed(2)
-            });
-        })
+		this.map.on('move', () => {
+			this.setState({
+				lng: this.map.getCenter().lng.toFixed(4),
+				lat: this.map.getCenter().lat.toFixed(4),
+				zoom: this.map.getZoom().toFixed(2)
+			});
+		})
 
-    }
+	}
 
-    // TODO- this is just for some test data, but we'll probably want some type of structure like this
-    getMapPointData() {
-        return {
-            'type': 'Feature',
-            'features': [
-                {
-                    'properties': {
-                        'id': 1,
-                        'label': 'House 1',
-                    },
-                    'geometry': {
-                        'type': 'Point',
-                        'coordinates': [-78.836754, 40.242299], // Note: long, lat
-                    }
-                },
-                {
-                    'properties': {
-                        'id': 2,
-                        'label': 'Building 2',
-                    },
-                    'geometry': {
-                        'type': 'Point',
-                        'coordinates': [-78.828488, 40.229427], // Note: long, lat
-                    }
-                },
-            ]
-        };
-    }
+	// TODO- this is just for some test data, but we'll probably want some type of structure like this
+	getMapPointData() {
+		return {
+			'type': 'Feature',
+			'features': [
+				{
+					'properties': {
+						'id': 1,
+						'label': 'House 1',
+					},
+					'geometry': {
+						'type': 'Point',
+						'coordinates': [-78.836754, 40.242299], // Note: long, lat
+					}
+				},
+				{
+					'properties': {
+						'id': 2,
+						'label': 'Building 2',
+					},
+					'geometry': {
+						'type': 'Point',
+						'coordinates': [-78.828488, 40.229427], // Note: long, lat
+					}
+				},
+			]
+		};
+	}
 
-    render() {
-        return (
-            <div>
-                <div id="geocoder" className="geocoder"></div>
-                <div ref={el => this.mapContainer = el} className="mapContainer" > </div>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<div id="geocoder" className="geocoder"></div>
+				<div ref={el => this.mapContainer = el} className="mapContainer" > </div>
+			</div>
+		);
+	}
 
 }
 
